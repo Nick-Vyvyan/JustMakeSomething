0b6a0a9afd45c86cc3d029f731b80c2b1757c85b
Nick-Vyvyan <vyvyannick@gmail.com>
2024-06-19 16:52:19 -0700

    Remove Empty Project

--- Source/JustMakeSomething/JustMakeSomething.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "JustMakeSomething.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JustMakeSomething, "JustMakeSomething" );
