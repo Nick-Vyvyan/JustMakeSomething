dffe9c912b3212f6064fd3e88f752555b4a91e0e
Nick-Vyvyan <vyvyannick@gmail.com>
2024-06-25 14:17:18 -0700

    new empty project

---
+++ SomethingElse/Source/SomethingElse/SomethingElse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SomethingElse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SomethingElse, "SomethingElse" );
